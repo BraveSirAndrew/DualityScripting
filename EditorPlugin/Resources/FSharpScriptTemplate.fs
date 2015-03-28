c27726c8c0aede118c00c1b61e245e0d00ffa192
Andrea <roundcrisis@gmail.com>
2015-03-28 20:06:15 +0000

    update f# template so that it uses namespace instead of module

--- EditorPlugin/Resources/FSharpScriptTemplate.fs
+++ EditorPlugin/Resources/FSharpScriptTemplate.fs
@@ -1,10 +1,10 @@
-﻿module Dualityscript
+﻿namespace Dualityscript
 
 open ScriptingPlugin
 open Duality
 
-    type FSharpScript() =
-        inherit DualityScript()
-        
-            override this.Update () =
-                printfn "updated"
+type FSharpScript() =
+    inherit DualityScript()
+    
+        override this.Update () =
+            Log.Editor.Write "updated"
