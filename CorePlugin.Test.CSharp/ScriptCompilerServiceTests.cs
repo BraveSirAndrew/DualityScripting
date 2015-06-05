63c3c7b839bd89aeda47d0f227a6e81ebb0207d1
Andrea <roundcrisis@gmail.com>
2015-06-05 21:15:35 +0100

    Adding a test to cover compile with compilationUnit

--- CorePlugin.Test.CSharp/ScriptCompilerServiceTests.cs
+++ CorePlugin.Test.CSharp/ScriptCompilerServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections;
 using Moq;
 using NUnit.Framework;
 using ScriptingPlugin;
@@ -8,18 +9,37 @@ namespace CorePlugin.Test.CSharp
 	[TestFixture]
     public class ScriptCompilerServiceTests
     {
-		[Test]
-		public void TryCompileDoesntThrowWhenErrorMessageContainsBraces()
-		{
-			var compiler = new Mock<IScriptCompiler>();
-			var compilerResult = new Mock<IScriptCompilerResults>();
-		    
-			compilerResult.Setup(m => m.Errors).Returns(new[] {"{", "}"});
-			compiler.Setup(m => m.Compile(It.IsAny<string>(), It.IsAny<string>())).Returns(compilerResult.Object);
+		private Mock<IScriptCompilerResults> _compilerResult;
+		private Mock<IScriptCompiler> _compiler;
 
-			var service = new ScriptCompilerService(compiler.Object);
+		[SetUp]
+		public void Setup()
+		{
+			_compiler = new Mock<IScriptCompiler>();
+			_compilerResult = new Mock<IScriptCompilerResults>();
+			_compilerResult.Setup(m => m.Errors).Returns(new[] { "{", "}" });
+		}
+
+		[Test]
+		public void TryCompileOneDoesntThrowWhenErrorMessageContainsBraces()
+		{
+			_compiler.Setup(m => m.Compile(It.IsAny<string>(), It.IsAny<string>())).Returns(_compilerResult.Object);
+
+			var service = new ScriptCompilerService(_compiler.Object);
 
 			Assert.DoesNotThrow(() => service.TryCompile("", "", ""));
 		}
+
+		[Test]
+		public void TryCompileManyDoesntThrowWhenErrorMessageContainsBraces()
+		{
+			_compiler.Setup(m => m.Compile(It.IsAny<CompilationUnit[]>(), It.IsAny<string>())).Returns(_compilerResult.Object);
+
+			var service = new ScriptCompilerService(_compiler.Object);
+
+			Assert.DoesNotThrow(() => service.TryCompile("", "", ""));
+		}
+
+		
     }
 }
